2fa2ba02a123680e8e31ab29cbf955d8eb32dce5
Azran14 <arnaud.dumarche@hetic.net>
2020-04-15 12:00:27 +0200

    links

--- src/script/popup.js
+++ src/script/popup.js
@@ -2,7 +2,7 @@ let films = document.querySelectorAll(".poster");
 let popup = document.querySelector(".popup");
 for (let i = 0; i < films.length; i++) {
   let film = films[i];
-  film.addEventListener("click", function() {
+  film.addEventListener("click", function () {
     popup.classList.toggle("is-open");
   });
 }
